3f5c630a8e8d2e949f807a037d0108aac904ecd3
lemmenn <nina.lemmens@hva.nl>
2023-11-14 13:27:06 +0100

    Task 1 - pre-processing and visualisation

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def main():
+    # TASK 1
+    load_data()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # TASK 2
+
+    # TASK 3
+
+    # TASK 4
+
+    # TASK 5
+
+    # TASK 6
+
+    # TASK 7
+
+
+# TASK 1
+def load_data():
+    shoe_labels = [5, 7, 9]
+    data = pd.read_csv("fashion-mnist_train.csv")
+
+    # load all sandals (5), sneakers (7) and ankle boots (9) from the dataset
+    shoes = data[data["label"].isin(shoe_labels)]
+
+    # separate labels and feature vectors
+    labels = shoes["label"]
+    features = shoes.drop("label", axis=1)
+
+    # display 1 image for each class
+    pixels_sandals = features.iloc[[(labels == 5).argmax()]].to_numpy().reshape((28, 28))
+    pixels_sneakers = features.iloc[[(labels == 7).argmax()]].to_numpy().reshape((28, 28))
+    pixels_boots = features.iloc[[(labels == 9).argmax()]].to_numpy().reshape((28, 28))
+
+    plt.imshow(pixels_sandals, cmap='gray')
+    plt.show()
+    plt.imshow(pixels_sneakers, cmap='gray')
+    plt.show()
+    plt.imshow(pixels_boots, cmap='gray')
+    plt.show()
+
+
+main()
